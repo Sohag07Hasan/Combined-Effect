2f25d7087981436bd3bf2fb64e44042923b1a857
sohag07hasan <hyde.sohag@gmail.com>
2013-07-20 02:26:48 +0600

    working

--- combined-effect/js/controller-script.js
+++ combined-effect/js/controller-script.js
@@ -1,19 +1,45 @@
 jQuery(document).ready(function($){
 	$("#modal-starter").leanModal({ top : 200, overlay : 0.4, closeButton: ".modal_close" });
 	
-	$('.flip-container').bind('click', function(){
-		
+	//navigation
+	$('.nav-image').bind('click', function(){
+		$('img.nav-image').removeClass('nav-selected');
+		$(this).addClass('nav-selected');
+		var nav_source = $(this).attr('src');
+		$('#flip-big-image').attr('src', nav_source);
+	});
+	
+	
+	//flip and revert
+	
+	
+	$('.flip-flip').bind('click', function(){		
 		$('.front').css({'background-color':'#BFBFBF'});
+		
+		var img_src = $('img#flip-big-image').attr('src');
+		$('input[name="selected-image-src"]').attr('value', img_src);
+		
+		var form_content = $('.back').html();
 		
 		$(".front").flip({
 			direction:'lr',
 			color: '#BFBFBF',
-			speed: 800,
+			speed: 100,
 			//content:'<img class="flip-image" src="./images/ripemangos.jpg" alt="Unavailable" />',
-			content: "Form content will goes here"
+			content: form_content
 		});
-	
+		
+		//undo flipping
+		$('.flip-revert').bind('click', function(){
+			$('.front').css({'background':'none'});
+			$('.front').revertFlip();
+		});
+		
+		return false;
 				
 	});
+	
+	
+	
 	
 });
